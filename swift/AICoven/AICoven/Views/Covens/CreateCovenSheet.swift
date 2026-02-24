5fe2844632e81ce07a12e0efcb4854e783cb34ec
Andreea Papillon <andreeapapillon@gmail.com>
2026-02-24 12:23:12 +0700

    Fix SwiftLint CI violations (redundant Sendable and invalid ViewBuilder debug log)

--- swift/AICoven/AICoven/Views/Covens/CreateCovenSheet.swift
+++ swift/AICoven/AICoven/Views/Covens/CreateCovenSheet.swift
@@ -15,9 +15,6 @@ struct CreateCovenSheet: View {
     let onCreated: (Coven) -> Void
 
     var body: some View {
-        // Debug: Log entitlement state to diagnose platform differences
-        _ = print("🏪 CreateCovenSheet: hasCreator=\(storeService.hasCreator), purchasedIDs=\(storeService.purchasedProductIDs)")
-
         if !storeService.hasCreator {
             FeatureUpsellView(
                 feature: .covens,
